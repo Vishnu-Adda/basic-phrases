9d9bbc527664873408c3c3723351dfa213640606
Vishnu Adda <addavishnu@gmail.com>
2018-05-27 23:35:15 -0700

    Adjust the XML and overall layout. Gave all children of the GridLayout some tags.

--- app/src/main/java/com/someapp/vishnu/mybasicphrases/MainActivity.java
+++ app/src/main/java/com/someapp/vishnu/mybasicphrases/MainActivity.java
@@ -2,8 +2,15 @@ package com.someapp.vishnu.mybasicphrases;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onClick(View view) {
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
