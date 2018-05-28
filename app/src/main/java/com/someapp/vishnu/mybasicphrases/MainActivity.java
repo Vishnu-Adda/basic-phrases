356a047312c39283e6dff05db65bff678165e303
Vishnu Adda <addavishnu@gmail.com>
2018-05-27 23:53:37 -0700

    Completed the algorithm for playing a specific audio for each button.

--- app/src/main/java/com/someapp/vishnu/mybasicphrases/MainActivity.java
+++ app/src/main/java/com/someapp/vishnu/mybasicphrases/MainActivity.java
@@ -1,14 +1,34 @@
 package com.someapp.vishnu.mybasicphrases;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    final int[] sounds = {R.raw.doyouspeakenglish,
+                    R.raw.goodevening,
+                    R.raw.hello,
+                    R.raw.howareyou,
+                    R.raw.ilivein,
+                    R.raw.mynameis,
+                    R.raw.please,
+                    R.raw.welcome};
+
+    MediaPlayer mediaPlayer;
+
     public void onClick(View view) {
 
+        int tag = Integer.parseInt(view.getTag().toString());
 
+        Toast.makeText(this, Integer.toString(tag), Toast.LENGTH_SHORT).show();
+
+        mediaPlayer = MediaPlayer.create(this, sounds[tag]);
+        mediaPlayer.start();
 
     }
 
